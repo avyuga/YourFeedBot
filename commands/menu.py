075829cb68026cdd73cab0a2af96523250a72f5d
Anastasia Vyugina <75266068+avyuga@users.noreply.github.com>
2022-05-09 20:17:22 +0300

    some refactoring x2

--- commands/menu.py
+++ commands/menu.py
@@ -1,10 +1,37 @@
 from aiogram import Dispatcher, types
 
-async def send_any_command_answer(callback: types.CallbackQuery):
+
+async def add_channel_from_button(callback: types.CallbackQuery):
     await callback.answer()
-    await callback.message.answer(f"You pressed the {callback.data} button")
+    await callback.message.answer(f"You pressed the ADD button")
+    await callback.message.answer('You are going to add channels here, but later')
+
+
+async def add_channel_from_command(message: types.Message):
+    await message.answer("This section is not ready yet. \nPlease wait!")
+
+
+async def get_settings_from_button(callback: types.CallbackQuery):
+    await callback.answer()
+    await callback.message.answer(f"You pressed the SETTINGS button")
+    await callback.message.answer('You are going to get settings list, but later')
+
+async def get_settings_from_command(message: types.Message):
+    await message.answer("This section is not ready yet. \nPlease wait!")
+
+# попытка пересылки
+# async def command_send_forward(message: types.Message):
+#     # await bot.forward_message(, , message.message_id)
+#     await message.answer("This section is not ready yet. \nPlease wait!")
+# dp.register_message_handler(command_send_forward, commands='forward', state='*')
 
 def register_menu_commands(dp: Dispatcher):
-    dp.register_callback_query_handler(send_any_command_answer,
-                                       text=['add_channel', 'get_settings'],
-                                       state='*')
+    dp.register_callback_query_handler(add_channel_from_button,
+                                       text='add_channel', state='*')
+    dp.register_message_handler(add_channel_from_command, commands='add_channel', state='*')
+
+    dp.register_callback_query_handler(get_settings_from_button,
+                                       text='get_settings', state='*')
+    dp.register_message_handler(get_settings_from_command, commands='settings', state='*')
+
+
--- main.py
+++ main.py
@@ -31,17 +31,6 @@ user_id = ""
 basic.register_basic_commands(dp)
 menu.register_menu_commands(dp)
 
-# ----------KEYBOARD--------------------------#
-
-@dp.callback_query_handler(lambda c: c.data == 'add_channel')
-async def process_callback_add_channel(callback_query: types.CallbackQuery):
-    await callback_query.answer()
-    await bot.send_message(callback_query.from_user.id, 'You are going to add a channel. '
-                                                        '\nPlease write channel names, starting from @, in a next '
-                                                        'message.')
-    await Form.WAIT_MESSAGE.set()
-
-
 async def parse_channels(message: types.Message, state: FSMContext):
     channels = re.findall(r'@\w*\b', message.text)
     print("Channels ", channels)
@@ -49,31 +38,6 @@ async def parse_channels(message: types.Message, state: FSMContext):
     print("User_data: ", user_data)
     await message.answer("First channel is: ", channels[0])
     await state.reset_state(with_data=False)
-
-# @dp.message_handler(state=Form.WAIT_MESSAGE)
-# async def parse_and_insert_channels(message: types.Message, state: FSMContext):
-#     channels = re.findall(r'@\w*\b', message.text)
-#     print(channels)
-#     await Form.READY.set()
-
-
-@dp.callback_query_handler(lambda c: c.data == 'get_settings')
-async def process_callback_show_settings(callback_query: types.CallbackQuery):
-    await callback_query.answer()
-    await bot.send_message(callback_query.from_user.id, 'You are going to get settings')
-
-
-# ----------COMMANDS---------------#
-
-@dp.message_handler(commands=['forward'])
-async def send_forward(message: types.Message):
-    # await bot.forward_message(, , message.message_id)
-    await message.reply("if you see this, it's not forwarded yet")
-
-
-@dp.message_handler(commands=['add_channel'])
-async def add_channel(message: types.Message):
-    await message.answer("This section is not ready yet. \nPlease wait!")
 
 
 @dp.message_handler()
@@ -97,12 +61,12 @@ if __name__ == '__main__':
     if connection is None or cursor is None:
         exit(1)
 
-    commands = [
-        BotCommand(command="/start", description="Start"),
-        BotCommand(command="/menu", description="Call for starting menu"),
-        BotCommand(command="/add", description="Add a channel"),
-        BotCommand(command="/settings", description="Go to settings")
-    ]
+    # commands = [
+    #     BotCommand(command="/start", description="Start"),
+    #     BotCommand(command="/menu", description="Call for starting menu"),
+    #     BotCommand(command="/add", description="Add a channel"),
+    #     BotCommand(command="/settings", description="Go to settings")
+    # ]
     # bot.set_my_commands(commands)
 
     executor.start_polling(dp, skip_updates=True)
